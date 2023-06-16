f9d0c79c34880075130834b7f8edce3cd1d85273
yeeeeechen <linryry@hotmail.com>
2023-06-16 22:37:52 +0800

    perturb swap bugfix, REMOVED aspectratio, change resizeSoftBlock

--- Node.h
+++ Node.h
@@ -10,16 +10,17 @@ struct Node{
     uint rightChild;
     uint parent;
     
-    double aspectRatio; // w/h
+    uint width;
+    uint height;
 
-    Node(): leftChild(0), rightChild(0), parent(0), aspectRatio(1.0){}
-    Node(uint l, uint r, uint p, double a = 1.0): leftChild(l), rightChild(r), parent(p), aspectRatio(a){}
+    Node(): leftChild(0), rightChild(0), parent(0), width(0), height(0){}
+    Node(uint l, uint r, uint p, uint w, uint h): leftChild(l), rightChild(r), parent(p), width(w), height(h){}
 
-    void setLeftChild(uint l){ leftChild = l; }
-    void setRightChild(uint r){ rightChild = r; }
-    void setParent(uint p){ parent = p; }
-    void rotate(){ aspectRatio = 1.0/aspectRatio; }
-    void setAspectRatio(double a){ aspectRatio = a; }
+    void setLeftChild(uint l);
+    void setRightChild(uint r);
+    void setParent(uint p);
+    void rotate();
+    void resize(uint w, uint h);
 };
 
 
